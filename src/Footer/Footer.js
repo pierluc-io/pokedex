c708bd043086f60392db69b5db9b0d479e59a6da
Pier-Luc Gendreau <pierluc@outlook.com>
2016-10-16 14:49:44 -0400

    Clean up footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,9 +5,7 @@ class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-        <p>Feat. Azure DocumentDB, Azure Functions, Azure Search & React</p>
-        <p><a href="https://github.com/pierluc-io/pokedex">Code available on GitHub</a></p>
-        <p>MIT &copy; <a href="https://www.pierluc.io/">Pier-Luc Gendreau</a></p>
+        <a href="https://github.com/pierluc-io/pokedex">Contribute on GitHub</a>
       </div>
     );
   }
